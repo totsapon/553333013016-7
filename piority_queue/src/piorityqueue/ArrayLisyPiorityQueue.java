406e2350eb8322c9eb864d1fe5701c2940d5f1c5
peypey004@gmail.com <peypey004@gmail.com>
2014-05-12 21:40:53 +0700

    Binarytree

---
+++ piority_queue/src/piorityqueue/ArrayLisyPiorityQueue.java
@@ -0,0 +1,28 @@
+package piorityqueue;
+
+import arraylist.ArrayList;
+
+public class ArrayLisyPiorityQueue {
+	
+	ArrayList lst = new ArrayList();
+	public void enQueue(Object e) {
+		lst.add(e);
+	}
+	public Object deQueue() {	
+		Object a = lst.get(maxIndex());
+		lst.remove(maxIndex());
+		return a;
+	}
+	private int maxIndex() {
+	       int max = 0;
+	       for (int i = 0; i < lst.size(); i++) {
+	                 Comparable d = (Comparable) lst.get(i);
+	                 Object maxObj = lst.get(max);
+	                 if (d.compareTo(maxObj) > 0) {
+	                            max = i;
+	                 }
+	       }
+	       return max;
+	}
+
+}
---
+++ piority_queue/src/piorityqueue/Test.java
@@ -0,0 +1,18 @@
+package piorityqueue;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BinaryHeap bh = new BinaryHeap();
+		bh.enQueue(13);
+		bh.enQueue(40);
+		bh.enQueue(50);
+		bh.enQueue(60);
+		bh.enQueue(70);
+		System.out.println(bh);
+		
+	}
+
+}
